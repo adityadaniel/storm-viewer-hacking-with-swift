b6621c6708060ce139593cae8c706ab64407d1b5
Daniel Aditya Istyana <adityadaniel@Daniels-MacBook-Pro.local>
2018-05-30 17:24:05 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#  Project 1 from HackingWithSwift book
+
+I started to learn iOS development and this is the complete source code from project 1.
+
---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Daniel Aditya Istyana on 5/26/18.
+//  Copyright © 2018 Daniel Aditya Istyana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        print(indexPath.row)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+
+}
+
